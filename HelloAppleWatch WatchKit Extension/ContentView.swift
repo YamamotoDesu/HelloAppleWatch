f1ea39ef713a1fb0534a42be89af4e4af0a56c0b
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2022-08-20 11:40:12 +0900

    Initial Commit

---
+++ HelloAppleWatch WatchKit Extension/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  HelloAppleWatch WatchKit Extension
+//
+//  Created by 山本響 on 2022/08/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @StateObject private var sentence = EmojiSentence()
+    
+    var body: some View {
+        VStack {
+            Image("Cookie")
+                .resizable()
+                .scaledToFit()
+                .overlay(
+                    Text(sentence.emoji)
+                        .font(.title3)
+                        .padding(.top, 10)
+                        .buttonStyle(.plain)
+                )
+            Text(sentence.text)
+                .font(.caption)
+                .padding(.top, 20)
+        }
+        .onTapGesture {
+            sentence.next()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HelloAppleWatch WatchKit Extension/HelloAppleWatchApp.swift
@@ -0,0 +1,19 @@
+//
+//  HelloAppleWatchApp.swift
+//  HelloAppleWatch WatchKit Extension
+//
+//  Created by 山本響 on 2022/08/20.
+//
+
+import SwiftUI
+
+@main
+struct HelloAppleWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
